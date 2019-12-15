ce79c4e032ed4453a7db808afa1aa20466161d40
Cole Arthur Parham <coleparham@gmail.com>
2019-12-15 16:12:24 +0000

    Automatically backed up by Learn

--- randomize.rb
+++ randomize.rb
@@ -4,5 +4,10 @@ def make_pairs(roster_array)
   roster_array = roster_array.shuffle
 
   array_of_pairs.push(roster_array.shift(2))
-
+  while roster_array.length > 0 do
+    array_of_pairs.push(roster_array.shift(2))
+  end
   return array_of_pairs
+end
+final_result = make_pairs(num_of_students)
+puts final_result
